587f26c83ec8d2f1c84da4eeb34b0064b18c803a
Pablo Táboas <pablo.taboas@bbva.com>
2017-06-23 00:03:46 +0200

    Fix: deploy stage

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" = 'master' ]; then
-    mvn deploy:deploy --settings=./.travis/settings.xml
+    mvn jar:jar deploy:deploy --settings=./.travis/settings.xml
 fi
