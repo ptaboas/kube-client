2c4ab2b54504c21b940540886e3fc616f48a7ac4
Pablo Táboas <pablo.taboas@bbva.com>
2017-08-02 11:37:57 +0200

    ass debug traces in release script

--- .travis/release.sh
+++ .travis/release.sh
@@ -1,26 +1,26 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-setup_git() {
-  git config --global user.email "travis@travis-ci.org"
-  git config --global user.name "Travis CI"
-}
-
-commit_changes() {
-  git add --all
-  git commit --message "$1"
-}
-
-upload_files() {
-  git remote add origin https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git
-  git push --quiet origin master
-}
-
-increment_version () { 
-	a=( ${1//./ } )
-	((a[2]++))
- 	DEVELOPMENT_VERISON="${a[0]}.${a[1]}.${a[2]}-SNAPSHOT"
- 	mvn versions:set -DnewVersion=$DEVELOPMENT_VERISON -DgenerateBackupPoms=false
-}
+#setup_git() {
+#  git config --global user.email "travis@travis-ci.org"
+#  git config --global user.name "Travis CI"
+#}
+#
+#commit_changes() {
+#  git add --all
+#  git commit --message "$1"
+#}
+#
+#upload_files() {
+#  git remote add origin https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git
+#  git push --quiet origin master
+#}
+#
+#increment_version () { 
+#	a=( ${1//./ } )
+#	((a[2]++))
+# 	DEVELOPMENT_VERISON="${a[0]}.${a[1]}.${a[2]}-SNAPSHOT"
+# 	mvn versions:set -DnewVersion=$DEVELOPMENT_VERISON -DgenerateBackupPoms=false
+#}
 
 echo "Gettin release version"
 RELEASE_VERSION=$(mvn help:evaluate -Dexpression=project.version | grep -v '\[' | sed 's/-SNAPSHOT$//')
