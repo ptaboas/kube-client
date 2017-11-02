fd1bc970898bb3eff4352143765b0cc8d46c8e5d
Pablo Táboas <pablo.taboas@bbva.com>
2017-11-02 01:00:50 +0100

    Fix build secrets reference

--- .travis/release.sh
+++ .travis/release.sh
@@ -4,7 +4,7 @@ VERSION=$(echo "$TRAVIS_TAG" | sed 's/^v//g')
 echo "Release version $VERSION"
 mvn versions:set -DnewVersion=$VERSION -DgenerateBackupPoms=false
 
-openssl aes-256-cbc -K $encrypted_fd725cdc679b_key -iv $encrypted_fd725cdc679b_iv -in ./.travis/codesigning.asc.enc -out codesigning.asc -d
+openssl aes-256-cbc -K $encrypted_85c50a6b4452_key -iv $encrypted_85c50a6b4452_iv -in ./.travis/codesigning.asc.enc -out codesigning.asc -d
 gpg --fast-import codesigning.asc
 
 mvn --settings=./.travis/settings.xml compile jar:jar source:jar-no-fork javadoc:jar gpg:sign deploy:deploy
