8991ab19bc3da84d81a705ab2d5b61d3d0ed3f0a
Pablo Táboas <pablo.taboas@bbva.com>
2018-05-05 22:02:26 +0200

    Add host ingress support

--- src/main/java/com/simplyti/cloud/kube/client/ingresses/IngressTlsCreationBuilder.java
+++ src/main/java/com/simplyti/cloud/kube/client/ingresses/IngressTlsCreationBuilder.java
@@ -1,11 +1,15 @@
 package com.simplyti.cloud.kube.client.ingresses;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.simplyti.cloud.kube.client.domain.IngressTls;
 
 public class IngressTlsCreationBuilder {
 
 	private final DefaultIngressCreationBuilder builder;
 	private String secretName;
+	private List<String> hosts = new ArrayList<>();
 
 	public IngressTlsCreationBuilder(DefaultIngressCreationBuilder builder) {
 		this.builder=builder;
@@ -15,9 +19,14 @@ public class IngressTlsCreationBuilder {
 		this.secretName=secretName;
 		return this;
 	}
+	
+	public IngressTlsCreationBuilder withHost(String host) {
+		hosts.add(host);
+		return this;
+	}
 
 	public DefaultIngressCreationBuilder create() {
-		return builder.addTls(new IngressTls(null, secretName));
+		return builder.addTls(new IngressTls(hosts, secretName));
 	}
 
 }
